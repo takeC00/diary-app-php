2d0ebbd34b9ed38da16cd9243fd070377669d098
takeC00 <take.614@icloud.com>
2026-03-25 12:56:38 +0900

    MAMP移行

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <title>DIARY-APP-PHP</title>
+    <style>
+    </style>
+  </head>
+  <body>
+  </body>
+</html>
