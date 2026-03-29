f94051064aee578b3e13aa5e05ac89924e095fb0
takeC00 <take.614@icloud.com>
2026-03-29 11:46:19 +0900

    ページネーション部分のコンポーネント化。初回アクセスの/にも対応

--- app/views/diaries/index.php
+++ app/views/diaries/index.php
@@ -10,40 +10,7 @@
 					<?php endforeach; ?>
 				</div>
 			</div>
-			<!-- ページが2以上ならページネーション -->
-			<?php if ($totalPages > 1): ?>
-				<div class="pagination">
-
-					<!-- 1ページ目は戻るボタン無効化 -->
-					<?php $page == 1 ? $gray='gray' : $gray = '' ;?>
-					<button class="page-button arrow <?= $gray ?>">
-						<?php if(!$gray) :?>
-							<a href="?page=<?= $_GET['page'] - 1 ?>"> < </a>
-						<?php else: ?>
-							<
-						<?php endif; ?>
-					</button>
-
-					<?php for ($i = 1; $i <= $totalPages; $i++): ?>
-						<?php if ($page === $i): ?>
-							<button class="page-button current"><?= $i ?></button>
-						<?php else: ?>
-							<a href="?page=<?= $i ?>"><button class="page-button"><?= $i ?></button></a>
-						<?php endif; ?>
-					<?php endfor; ?>
-
-					<!-- 最終ページ目は進むボタン無効化 -->
-					<?php $page == $totalPages ? $gray='gray' : $gray = '' ;?>
-					<button class="page-button arrow <?= $gray ?>">
-					<?php if(!$gray) :?>
-							<a href="?page=<?= $_GET['page'] + 1 ?>"> > </a>
-						<?php else: ?>
-							>
-						<?php endif; ?>
-					</button>
-
-				</div>
-			<?php endif; ?>
+			<?php require('../app/views/components/common/pagination.php') ?>
 		</section>
 	</main>
 </body>
